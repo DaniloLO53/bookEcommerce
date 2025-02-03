22d7594fb9b35babcce20abf66825a34199d7684
DaniloLO53 <daniloleaodev@gmail.com>
2025-02-03 08:58:50 -0300

    feat: find user by name

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -3,14 +3,33 @@ package repository;
 import database.DatabaseConnection;
 import domain.User;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
+    public static List<User> findByName(String name) {
+        String query = "SELECT * FROM users WHERE CONCAT(first_name, last_name) ILIKE ?";
+        List<User> users = new ArrayList<>();
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ) {
+            preparedStatement.setString(1, "%" + name + "%");
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    User user = buildUserByResultSet(resultSet);
+                    users.add(user);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
     public static List<User> findAll() {
         String query = "SELECT * FROM users";
         List<User> users = new ArrayList<>();
@@ -19,21 +38,24 @@ public class UserRepository {
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query);
         ) {
-
             while (resultSet.next()) {
-                User user = User
-                        .builder()
-                        .id(resultSet.getInt("id"))
-                        .email((resultSet.getString("email")))
-                        .firstName((resultSet.getString("first_name")))
-                        .lastName((resultSet.getString("last_name")))
-                        .password((resultSet.getString("password")))
-                        .build();
+                User user = buildUserByResultSet(resultSet);
                 users.add(user);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return users;
+    }
+
+    private static User buildUserByResultSet(ResultSet resultSet) throws SQLException {
+        return User
+                .builder()
+                .id(resultSet.getInt("id"))
+                .email((resultSet.getString("email")))
+                .firstName((resultSet.getString("first_name")))
+                .lastName((resultSet.getString("last_name")))
+                .password((resultSet.getString("password")))
+                .build();
     }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class UserService {
     public static void printAll() {
-        List<User> users = UserRepository.findAll();
+        List<User> users = UserRepository.findByName("da");
 
         users.forEach(u -> System.out.printf("ID: %s | EMAIL: %s | FIRST NAME: %s | LAST NAME: %s | PASSWORD: %s%n",
                 u.getId(), u.getEmail(), u.getFirstName(), u.getLastName(), u.getPassword()));
