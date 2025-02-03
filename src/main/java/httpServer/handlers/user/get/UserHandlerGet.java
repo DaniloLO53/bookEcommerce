9873ad4ec60eafe0416089b1c00ee1261f9b2236
DaniloLO53 <daniloleaodev@gmail.com>
2025-02-03 13:57:42 -0300

    feat: partial user find implementation

--- src/main/java/httpServer/handlers/user/get/UserHandlerGet.java
+++ src/main/java/httpServer/handlers/user/get/UserHandlerGet.java
@@ -1,17 +1,32 @@
 package httpServer.handlers.user.get;
 
+import com.google.gson.Gson;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
+import domain.User;
 import service.UserService;
+
+import java.util.List;
 
 public class UserHandlerGet {
     public static void handle(HttpExchange exchange, String requestPath) {
         System.out.println("Request path: " + requestPath);
         if (requestPath.equals("/users") || requestPath.equals("/users/")) {
-            UserService.findAll();
+            findAll(exchange);
         }
     }
 
-    private void sendResponse(HttpExchange exchange, Integer statusCode, String responseBody) {
+    static void findAll(HttpExchange exchange) {
+        Gson gson = new Gson();
 
+        List<User> usersList = UserService.findAll();
+        String jsonArray = gson.toJson(usersList);
+
+        sendResponse(exchange, 200, jsonArray);
+    }
+
+    static void sendResponse(HttpExchange exchange, Integer statusCode, String responseBody) {
+        Headers responseHeaders = exchange.getResponseHeaders();
+        responseHeaders.set("Content-Type", "application/json; charset=UTF-8");
     }
 }
